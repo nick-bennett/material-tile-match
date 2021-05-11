c1008f74f75c3d33b418a3bfc572a9006b7b17aa
Nick Bennett <nick@nickbenn.com>
2021-05-11 16:24:50 -0600

    Tick timer stream working & displaying.

---
+++ lib/view/tile_images.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+List<IconData> tileImages = [
+  Icons.ac_unit,
+  Icons.pool,
+  Icons.camera_alt,
+  Icons.lock,
+  Icons.lock_open,
+  Icons.two_wheeler,
+  Icons.airplanemode_active,
+  Icons.airport_shuttle,
+  Icons.emoji_food_beverage,
+  Icons.wine_bar,
+  Icons.hardware,
+  Icons.delete,
+  Icons.wb_sunny,
+  Icons.monetization_on,
+  Icons.bakery_dining,
+  Icons.volume_up
+];
